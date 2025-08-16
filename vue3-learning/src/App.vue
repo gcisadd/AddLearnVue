7800f3ed24b12001646c33243c98c6f026cfbf49
gcisadd <2374113375@qq.com>
2025-08-16 22:25:50 +0800

    reactive 和 ref

--- vue3-learning/src/App.vue
+++ vue3-learning/src/App.vue
@@ -1,21 +1,40 @@
-<script>
+<!-- <script>
 import { ref } from 'vue'
-  export default {
-    setup() {
-      const msg = ref('Hello World!')
-      const fun = () => {
-        console.log('Hello World!')
-      }
-      return {
-        msg,
-        fun
-      }
+export default {
+  setup() {
+    const msg = ref('Hello World!')
+    const fun = () => {
+      console.log('Hello World!')
+    }
+    return {
+      msg,
+      fun
     }
   }
-
+}
+</script> -->
+<script setup>
+  import { reactive, ref } from 'vue';
+  const msg = ref('Hello World!')
+  const obj1 = reactive({
+    name: 'Vue3',
+    age: 18
+  })
+  const obj2 = {
+    name: 'Vue3',
+    age: 18
+  }
+  const fun = () => {
+    obj1.age ++
+    obj2.age ++
+    console.log(msg)
+  }
 </script>
 <template>
   <div>
+    <div>{{ msg }}</div>
+    <div>{{ obj1}}</div>
+    <div>{{ obj2}}</div>
     <button @click="fun">{{ msg }}</button>
   </div>
 </template>
